8de223a48edeaf25bd9b45a8aac1474156669ab0
Uday Raj <udayraj.vadeghar@gmail.com>
2024-12-23 20:43:14 +0530

    add async-handler , Carts Logic added

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import authRoute from "./routes/AUTH.route.js"; //make sure that .js in module type. idk why...?
+import cartRoute from "./routes/cart.route.js";
 import productsRoute from "./routes/products.route.js";
 import sellerDeatilsRoute from "./routes/sellerDetails.route.js";
 
@@ -15,6 +16,7 @@ app.use(express.json()); //future note : use body parser if api error in json pa
 app.use("/api/auth", authRoute); // route to manage the  route
 app.use("/api/products", productsRoute);
 app.use("/api/sellerDetails", sellerDeatilsRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
