e3c20af29a4e510791f36f0950d528b80f9788af
Uday Raj <udayraj.vadeghar@gmail.com>
2024-12-23 22:46:33 +0530

    Add cart_product Route

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import authRoute from "./routes/AUTH.route.js"; //make sure that .js in module type. idk why...?
 import cartRoute from "./routes/cart.route.js";
+import cartProductRoute from "./routes/cart_product.route.js";
 import productsRoute from "./routes/products.route.js";
 import sellerDeatilsRoute from "./routes/sellerDetails.route.js";
 
@@ -17,6 +18,7 @@ app.use("/api/auth", authRoute); // route to manage the  route
 app.use("/api/products", productsRoute);
 app.use("/api/sellerDetails", sellerDeatilsRoute);
 app.use("/api/carts", cartRoute);
+app.use("/api/cartProduct", cartProductRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
