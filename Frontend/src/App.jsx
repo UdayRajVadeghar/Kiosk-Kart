a1a829049b21dedf7067ad526afe02df594757cf
Uday Raj <udayraj.vadeghar@gmail.com>
2024-12-24 15:59:20 +0530

    Add routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 const App = () => {
-  return <div className="">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
+
 export default App;
