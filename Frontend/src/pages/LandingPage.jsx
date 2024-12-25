1d688057bee6c89cb3ea9e584798d5bd76cac706
Uday Raj <udayraj.vadeghar@gmail.com>
2024-12-25 19:45:52 +0530

    Add signup background

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 const LandingPage = () => {
-  return <div>Landing Page</div>;
+  return <div></div>;
 };
 
 export default LandingPage;
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,5 +1,11 @@
 const Signup = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="bg-landing bg-cover bg-center h-screen">
+      <div className="h-screen flex justify-center items-center">
+        <div className=""></div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
