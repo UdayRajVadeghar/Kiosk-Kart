1708378a3be44979f983618960d135ae57d132fb
Uday Raj <udayraj.vadeghar@gmail.com>
2024-12-26 01:38:41 +0530

    Checkpoint one :-)

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -9,6 +9,7 @@ const signUpSchema = z.object({
   name: z.string().min(1, "Name is required"),
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters"),
+  //add enum if required
 });
 
 const Signup = () => {
